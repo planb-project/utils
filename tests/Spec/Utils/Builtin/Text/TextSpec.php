34dd8f52c87dcd65f692b65775ccc3e4c313bfc7
jmpantoja <jmpantoja@gmail.com>
2019-04-20 14:33:45 +0200

    Issue #1. Refactoring en tests

--- tests/Spec/Utils/Builtin/Text/TextSpec.php
+++ tests/Spec/Utils/Builtin/Text/TextSpec.php
@@ -19,6 +19,24 @@ class TextSpec extends ObjectBehavior
     const GREETING_LENGTH = 29;
 
 
+    /**
+     * @param $text
+     */
+    private function make($format, ...$params): void
+    {
+
+        array_unshift($params, $format);
+        $this->beConstructedThrough('make', $params);
+    }
+
+    /**
+     * @param $text
+     */
+    private function makeEncoded($text, $encoding): void
+    {
+        $this->beConstructedThrough('makeEncoded', [$text, $encoding]);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(Text::class);
@@ -27,7 +45,7 @@ class TextSpec extends ObjectBehavior
 
     public function it_is_initializable_from_a_simple_string()
     {
-        $this->beConstructedThrough('make', [self::A_NAME]);
+        $this->make(self::A_NAME);
         $this->shouldHaveType(Text::class);
 
         $this->__toString()->shouldReturn(self::A_NAME);
@@ -35,7 +53,9 @@ class TextSpec extends ObjectBehavior
 
     public function it_is_initializable_from_a_format_string()
     {
-        $this->beConstructedThrough('make', [self::GREETING_FORMAT, self::A_NAME]);
+
+        $this->make(self::GREETING_FORMAT, self::A_NAME);
+
         $this->shouldHaveType(Text::class);
 
         $this->__toString()->shouldReturn(self::GREETING);
@@ -43,14 +63,14 @@ class TextSpec extends ObjectBehavior
 
     public function it_has_the_internal_encoding_by_default()
     {
-        $this->beConstructedThrough('make', [self::GREETING_FORMAT, self::A_NAME]);
+        $this->make(self::GREETING_FORMAT, self::A_NAME);
         $this->getEncoding()->shouldReturn(mb_internal_encoding());
     }
 
     public function it_is_initializable_with_a_specific_encoding()
     {
 
-        $this->beConstructedThrough('makeEncoded', [self::A_UTF_7_NAME, Encoding::UTF_7()]);
+        $this->makeEncoded(self::A_UTF_7_NAME, Encoding::UTF_7());
         $this->shouldHaveType(Text::class);
 
         $this->__toString()->shouldReturn(self::A_UTF_7_NAME);
@@ -58,7 +78,7 @@ class TextSpec extends ObjectBehavior
 
     public function it_can_change_the_encoding()
     {
-        $this->beConstructedThrough('makeEncoded', [self::A_UTF_7_NAME, Encoding::UTF_7()]);
+        $this->makeEncoded(self::A_UTF_7_NAME, Encoding::UTF_7());
 
         $this->changeEncoding(Encoding::UTF_8())
             ->__toString()->shouldReturn(self::A_NAME);
@@ -66,7 +86,7 @@ class TextSpec extends ObjectBehavior
 
     public function it_do_nothing_if_the_new_encoding_if_the_equal_to_current()
     {
-        $this->beConstructedThrough('makeEncoded', [self::A_UTF_7_NAME, Encoding::UTF_7()]);
+        $this->makeEncoded(self::A_UTF_7_NAME, Encoding::UTF_7());
 
         $this->changeEncoding(Encoding::UTF_7())
             ->__toString()->shouldReturn(self::A_UTF_7_NAME);
@@ -75,7 +95,7 @@ class TextSpec extends ObjectBehavior
 
     public function it_can_retrieve_the_length_of_a_text()
     {
-        $this->beConstructedThrough('make', [self::GREETING_FORMAT, self::A_NAME]);
+        $this->make(self::GREETING_FORMAT, self::A_NAME);
 
         $this->getLength()->shouldReturn(self::GREETING_LENGTH);
     }
@@ -91,37 +111,38 @@ class TextSpec extends ObjectBehavior
 
     public function it_identifies_an_empty_text()
     {
-        $this->beConstructedThrough('make', [Text::EMPTY]);
+        $this->make(Text::EMPTY);
         $this->isEmpty()->shouldReturn(true);
         $this->isBlank()->shouldReturn(true);
     }
 
     public function it_identifies_a_blank_text()
     {
-        $this->beConstructedThrough('make', [Text::SPACE]);
+        $this->make(Text::SPACE);
         $this->isEmpty()->shouldReturn(false);
         $this->isBlank()->shouldReturn(true);
     }
 
     public function it_identifies_a_printable_text()
     {
-        $this->beConstructedThrough('make', [self::GREETING]);
+        $this->make(self::GREETING);
         $this->isEmpty()->shouldReturn(false);
         $this->isBlank()->shouldReturn(false);
     }
 
     public function it_identifies_a_tab_char()
     {
-        $this->beConstructedThrough('make', ["\t"]);
+        $this->make("\t");
         $this->isEmpty()->shouldReturn(false);
         $this->isBlank()->shouldReturn(true);
     }
 
     public function it_identifies_a_new_line_char()
     {
-        $this->beConstructedThrough('make', ["\n"]);
+        $this->make("\n");
         $this->isEmpty()->shouldReturn(false);
         $this->isBlank()->shouldReturn(true);
     }
+
 
 }
